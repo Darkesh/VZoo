196de05f3f9502c23b4ec7401c9de1fa1357a8b3
Darkesh <59623562+Darkesh@users.noreply.github.com>
2020-06-04 14:24:36 +0100

    Initial commit

---
+++ src/main/java/com/example/VZoo/VZooApplication.java
@@ -0,0 +1,13 @@
+package com.example.VZoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VZooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VZooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VZoo/VZooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VZoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VZooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
